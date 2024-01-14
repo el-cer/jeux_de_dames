8d609e99eb6ea521e4c8f12956751d35ef67964e
eliot ceron <eliot-arana@outlook.com>
2024-01-14 17:59:50 +0100

    add of a new funnction named click whichpermit to click on a piece and click on a future valid position. add a function named change_turn which change the turn of between white and black.Modification of valid_moves and add of a new function named can_kill which allows the mouvement of a eat a enemy.Remove of kill_eemy function becaus now not necessary beacause before to moove the piece it was necessary  to do right click or left click. But whith the function click now wee ca click on the piece and click on the future position to kill. NEXT STEP ADD a draw circle on the future position.

--- code/back_game.py
+++ code/back_game.py
@@ -8,20 +8,26 @@ class Board:
         self.mat = None
         self.list_black = []
         self.list_white = []
+        self.list_kill_moove_black = None
+        self.list_kill_moove_white = None 
+        self.list_moove_black,self.list_moove_white = None,None
         self.moove_yes = False
         self.draw_future_position = True
         self.pion = True
         self.selected = None
+        self.moove =  None
+        self.state = None
+        self.yes = None
+        self.turn = None
 
         self.create_board()
-
     def create_board(self):
         self.mat = np.array([[2, 0, 2, 0, 2, 0, 2, 0],
                              [0, 2, 0, 2, 0, 2, 0, 2],
                              [2, 0, 2, 0, 2, 0, 2, 0],
                              [0, 0, 0, 0, 0, 0, 0, 0],
                              [0, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 1, 0, 1, 0, 1, 0, 1],
+                             [0, 1, 0, 1, 0, 1, 0, 1],                                                                                                                                                                                                                                
                              [1, 0, 1, 0, 1, 0, 1, 0],
                              [0, 1, 0, 1, 0, 1, 0, 1]])
         # Matrice de 8x8 avec Pour les 3 premiere lige des valeurs 2 pour noir et 1 pour blanc
@@ -30,6 +36,100 @@ class Board:
             for col in range(8):
                 color = WHITE if (row + col) % 2 == 0 else BLACK #pour chaque carre mettre 1 fois sur 2 blanc et noir
                 pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+      
+    def click(self, event, screen, row, col, click):
+        # Ignore events that are not mouse clicks
+        if self.selected is None and self.state is None:
+            if self.mat[row][col] == 1 or self.mat[row][col] == 2:
+
+                self.selected = (row, col)
+                pygame.draw.circle(screen, GREY, (self.selected[1]+1 * SQUARE_SIZE + SQUARE_SIZE // 2, self.selected[0]-1 * SQUARE_SIZE + SQUARE_SIZE // 2),10)
+                pygame.display.flip()
+                self.valid_moove()
+
+                self.can_kill()
+                print(self.list_kill_moove_black,self.list_moove_black)
+                
+                self.state = 'selected'
+        if self.state == "selected":
+            if self.list_kill_moove_black is None or self.list_kill_moove_white is None :
+            
+                # If the state is selected, check if the second click is valid
+                    # Check if the clicked position is (row-1, col+1)
+                
+                if  self.selected in self.list_white and (row, col) == (self.selected[0] - 1, self.selected[1] + 1) and self.selected is not None and (self.selected[0] - 1, self.selected[1] + 1) in self.list_moove_white:
+                
+                    self.mat[self.selected[0]][self.selected[1]] = 0
+                    self.mat[self.selected[0] - 1][self.selected[1] + 1] = 1
+                    self.state = None
+                    self.selected = None
+                    self.list_moove_white = None
+                if  self.selected in self.list_white and (row, col) == (self.selected[0] - 1, self.selected[1] - 1) and self.selected is not None and (self.selected[0] - 1, self.selected[1]- 1) in self.list_moove_white:
+
+                    self.mat[self.selected[0]][self.selected[1]] = 0
+                    self.mat[self.selected[0] - 1][self.selected[1] - 1] = 1
+                    self.state = None
+                    self.selected = None 
+                    self.list_moove_white = None
+
+                if  self.selected in self.list_black and (row, col) == (self.selected[0] + 1, self.selected[1] + 1) and self.selected is not None and (self.selected[0] + 1, self.selected[1] + 1) in self.list_moove_black:
+
+                    self.mat[self.selected[0]][self.selected[1]] = 0
+                    self.mat[self.selected[0] + 1][self.selected[1] + 1] = 2 
+                    self.state = None
+                    self.selected = None
+                    self.list_moove_black = None
+
+                if self.selected in self.list_black and (row, col) == (self.selected[0] + 1, self.selected[1] - 1) and self.selected is not None and (self.selected[0] + 1, self.selected[1] - 1) in self.list_moove_black:
+                    
+                    self.mat[self.selected[0]][self.selected[1]] = 0
+                    self.mat[self.selected[0] + 1][self.selected[1]- 1] = 2 
+                    self.state = None
+                    self.selected = None 
+                    self.list_moove_black = None
+
+            if self.list_kill_moove_white is not None or self.list_kill_moove_black is not None:
+                    if self.selected in self.list_white and (row, col) == (self.selected[0] - 2, self.selected[1] + 2) and self.selected is not None and (self.selected[0] - 2, self.selected[1] + 2) in self.list_kill_moove_white:            
+                        self.mat[self.selected[0]][self.selected[1]] = 0
+                        self.mat[self.selected[0] - 1][self.selected[1] + 1] = 0
+
+                        self.mat[self.selected[0] - 2][self.selected[1] + 2] = 1
+                        self.state = None
+                        self.selected = None 
+                        self.list_kill_moove_white = None
+
+
+                    if  self.selected in self.list_white and (row, col) == (self.selected[0] - 2, self.selected[1] -2) and self.selected is not None and (self.selected[0] - 2, self.selected[1]- 2) in self.list_kill_moove_white:            
+                        self.mat[self.selected[0]][self.selected[1]] = 0
+                        self.mat[self.selected[0] - 1][self.selected[1] - 1] = 0
+                        self.mat[self.selected[0] - 2][self.selected[1] - 2] = 1
+                        self.state = None
+                        self.selected = None
+                        self.list_kill_moove_white = None
+                    #BLACK
+                    if self.selected in self.list_black and (row, col) == (self.selected[0] + 2, self.selected[1] + 2) and self.selected is not None and (self.selected[0] + 2, self.selected[1] + 2) in self.list_kill_moove_black:            
+                        self.mat[self.selected[0]][self.selected[1]] = 0
+                        self.mat[self.selected[0] + 1][self.selected[1] + 1] = 0
+
+                        self.mat[self.selected[0] + 2][self.selected[1] + 2] = 2
+                        self.state = None
+                        self.selected = None 
+                        self.list_kill_moove_black = None
+                        
+
+                    if self.selected in self.list_black and (row, col) == (self.selected[0] + 2, self.selected[1] -2) and self.selected is not None and (self.selected[0] + 2, self.selected[1]- 2) in self.list_kill_moove_black:            
+                        self.mat[self.selected[0]][self.selected[1]] = 0
+                        self.mat[self.selected[0] + 1][self.selected[1] - 1] = 0
+
+                        self.mat[self.selected[0] + 2][self.selected[1] - 2] = 2
+                        self.state = None
+                        self.selected = None 
+                        self.list_kill_moove_black = None
+    def change_turn(self):
+        if self.turn is None:
+            self.turn = 1
+        else:
+            self.turn =None                
     def draw_pieces(self, screen):
 
         for row in range(8):
@@ -39,66 +139,68 @@ class Board:
                     
                 elif self.mat[row, col] == 1: #inverse
                     pygame.draw.circle(screen, (220, 220, 255), (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 20)
-
+                
     def sides(self):
         self.list_black = [(x, y) for x in range(self.mat.shape[0]) for y in range(self.mat.shape[1]) if self.mat[x][y] == 2]
         #mettre danns une list les coordonnées des pions noir 
         self.list_white = [(x, y) for x in range(self.mat.shape[0]) for y in range(self.mat.shape[1]) if self.mat[x][y] == 1]        
         
         self.list_empty = [(x, y) for x in range(self.mat.shape[0]) for y in range(self.mat.shape[1]) if self.mat[x][y] == 0]        
-
-    def valid_moove(self,row,col):
-        if (row,col) in self.list_white  and row-1>=0 and col+1<8 and col-1>=0:
-            self.list_moove_white =[(row-1,col+1),(row-1,col-1)]
-
-        elif (row,col) in self.list_black and row+1<8 and col+1<8 and col-1>=0:
-            self.list_moove_black = [(row+1,col-1),(row+1,col+1)]
-        
     
+
+    def valid_moove(self):
+        if  (self.selected[0],self.selected[1]) in self.list_white:
             
-
-        #pareil pour les blanc
-    def draw_valid_position(self,screen,row,col):
-        if self.valid_moove in self.list_empty:
-            pygame.draw.circle(screen, GREY, ((col + 1) * SQUARE_SIZE + SQUARE_SIZE // 2, (row - 1) * SQUARE_SIZE + SQUARE_SIZE // 2), 5)
-            pygame.draw.circle(screen, GREY, ((col - 1) * SQUARE_SIZE + SQUARE_SIZE // 2, (row - 1) * SQUARE_SIZE + SQUARE_SIZE // 2), 5)
-            pygame.display.flip() 
-             # Update the display to show the temporary circles
-        
-
-    def click_on_future(self,click, position, event, row, col):
-        pass
- 
-              
-    def moove(self,event,screen):
-        col = event.pos[0] // SQUARE_SIZE
-        row = event.pos[1] // SQUARE_SIZE
-        #a chaque click on a la position du pion
- 
-        if (row,col) in self.list_white and all(move in self.list_empty for move in self.list_moove_white):
-            print(True)
-            if event.button == 1:
-                    self.mat[row][col] -= 1
-                    self.mat[row - 1][col + 1] += 1
-                    print('1')
-                #si click gauche le pion va aller a droite
-            elif event.button == 3:
-                    self.mat[row][col] -= 1 
-                    self.mat[row - 1][col - 1] += 1
-                    
+            if  (self.selected[0]-1,self.selected[1]+1) in self.list_empty and (self.selected[0]-1,self.selected[1]-1) in self.list_empty: 
+                self.list_moove_white =[(self.selected[0]-1,self.selected[1]+1),(self.selected[0]-1,self.selected[1]-1)]
+            
+            elif (self.selected[0]-1,self.selected[1]+1) in self.list_white or (self.selected[0]-1,self.selected[1]-1) in self.list_empty or  self.selected[1]+1>=8 and self.selected[0]-1>=0 and self.selected[1]-1>=0  :
                 
-                #button =3 click droit c'est pour aller a droite
+                self.list_moove_white =[(self.selected[0]-1,self.selected[1]-1)]
             
+            elif (self.selected[0]-1,self.selected[1]-1) in self.list_white and  self.selected[0]-1>=0 and self.selected[1]+1>=0:
+                
+                self.list_moove_white =[(self.selected[0]-1,self.selected[1]+1)]  
+            else:
+                self.list_moove_white = None
+        elif   (self.selected[0],self.selected[1]) in self.list_black :
             
-        elif (row,col) in self.list_black and all(move in self.list_empty for move in self.list_moove_white):
-            print('can')
-            if event.button == 1 :
-                    self.mat[row][col] = 0
-                    self.mat[row + 1][col + 1] += 2                    
-            elif event.button == 3:
-                    self.mat[row][col] =0
-                    self.mat[row + 1][col -1] += 2
+            if   (self.selected[0]+1,self.selected[1]-1) in self.list_empty and (self.selected[0]+1,self.selected[1]+1) in self.list_empty and self.selected[0]+1<8 and self.selected[1]-1>=0 and self.selected[1]+1<8 and self.selected[1]-1>=0:
+              
+                self.list_moove_black = [(self.selected[0]+1,self.selected[1]-1),(self.selected[0]+1,self.selected[1]+1)]
+           
+            elif (self.selected[0]+1,self.selected[1]-1) in self.list_black or (self.selected[0]+1,self.selected[1]+1) in self.list_empty or self.selected[1]+1>=8 and self.selected[0]+1>8 and self.selected[1]+1>8:
             
+                self.list_moove_black =[(self.selected[0]+1,self.selected[1]+1)]
+            
+            elif (self.selected[0]+1,self.selected[1]+1) in self.list_black or (self.selected[0]+1,self.selected[1]-1) in self.list_empty  or self.selected[1]-1>=0 and self.selected[0]+1>8 :
+            
+                self.list_moove_black =[(self.selected[0]+1,self.selected[1]-1)]  
+            else:
+                self.list_moove_black = None
+    
+    def can_kill(self):
+
+        if (self.selected[0],self.selected[1]) in self.list_white:
+            
+                
+                if  self.mat[self.selected[0]-2][self.selected[1]-2] ==0 and (self.selected[0]-1,self.selected[1]-1) in self.list_black: 
+                    
+                    self.list_kill_moove_white =[(self.selected[0]-2,self.selected[1]-2)]
+                
+                else:
+                    self.list_kill_moove_white =[(self.selected[0]-2,self.selected[1]+2)]
+
+        elif (self.selected[0],self.selected[1]) in self.list_black and self.selected[0]+1<8 and self.selected[1]-1>=0 and self.selected[1]+1<8 and self.selected[1]-1>=0 and (self.selected[0]+1,self.selected[1]-1) in self.list_white or (self.selected[0]+1,self.selected[1]+1) in self.list_white:
+           
+            if   self.mat[self.selected[0]+2][self.selected[1]-2] ==0 and (self.selected[0]+1,self.selected[1]-1) in self.list_white:
+            
+                self.list_kill_moove_black =[(self.selected[0]+2,self.selected[1]-2)]
+            else:
+                self.list_kill_moove_black = [(self.selected[0]+2,self.selected[1]+2)]
+            
+    
+                    
     def remove(self):
         for x in range(0,self.mat.shape[0]):
             for y in range(0,self.mat.shape[1]):
@@ -133,6 +235,9 @@ class Board:
 
                 self.mat[row+2][col-2] += 2
                 self.mat[row+1,col-1] =0
+    
+
+
                 
             
 
