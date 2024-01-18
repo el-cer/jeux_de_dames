4f3eb897d08415ce2928ffc1f106d3f62701a1de
eliot ceron <eliot-arana@outlook.com>
2024-01-18 10:17:29 +0100

    resize the game 600 instead of 400, add new color BLUE,add new black piece picture and white piece picture, change of the graphic interface

--- code/config.py
+++ code/config.py
@@ -1,8 +1,30 @@
 
 
+import pygame 
+import os 
 
-WIDTH, HEIGHT = 400, 400
-SQUARE_SIZE = 50
+
+WIDTH, HEIGHT = 600, 600
+row,col = 8,8
+SQUARE_SIZE = WIDTH//row
+RADIUS = 30
 BLACK = (0,0, 0)
 WHITE = (255, 255, 255)
 GREY = (128,128,128)
+BLUE = (16,1,65)
+
+
+GREEN = (19,132,20)
+BROWN =(46,41,41)
+
+path = 'image'
+
+piece_black = pygame.image.load(os.path.join(path,'pion_noir.png'))
+piece_white = pygame.image.load(os.path.join(path,'pion_blanc.png'))
+        
+
+# Scale the image
+piece_black = pygame.transform.scale(piece_black, (SQUARE_SIZE, SQUARE_SIZE))
+piece_white = pygame.transform.scale(piece_white, (SQUARE_SIZE, SQUARE_SIZE))
+
+
