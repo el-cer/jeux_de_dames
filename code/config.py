2181a90ca964ae9b5975bb9b59b129ecd74ac6c8
eliot ceron <eliot-arana@outlook.com>
2024-01-31 14:58:00 +0100

    ajout des images

--- code/config.py
+++ code/config.py
@@ -7,8 +7,10 @@ import os
 WIDTH, HEIGHT = 600, 600
 row,col = 8,8
 SQUARE_SIZE = WIDTH//row
-RADIUS = 30
+RADIUS = 30 
 RADIUS_FUTURE_POSITION = 10
+
+#COLORSdm
 BLACK = (0,0, 0)
 WHITE = (255, 255, 255)
 GREY = (128,128,128)
@@ -22,10 +24,14 @@ path = 'image'
 
 piece_black = pygame.image.load(os.path.join(path,'pion_noir.png'))
 piece_white = pygame.image.load(os.path.join(path,'pion_blanc.png'))
+black_dame = pygame.image.load(os.path.join(path,'black_dame.png'))
+white_dame = pygame.image.load(os.path.join(path,'white_dame.png'))
         
 
 # Scale the image
 piece_black = pygame.transform.scale(piece_black, (SQUARE_SIZE, SQUARE_SIZE))
 piece_white = pygame.transform.scale(piece_white, (SQUARE_SIZE, SQUARE_SIZE))
+black_dame = pygame.transform.scale(black_dame, (SQUARE_SIZE, SQUARE_SIZE))
+white_dame = pygame.transform.scale(white_dame, (SQUARE_SIZE, SQUARE_SIZE))
 
 
